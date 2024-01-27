ca788b511fd2bd066db12cd7cce2f535dfb8f2fd
Abu Taher <writerabutaher@gmail.com>
2024-01-27 13:48:48 +0600

    setup project with redux

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import AllProvider from "@/Provider/AllProvider";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AllProvider>{children}</AllProvider>
+      </body>
     </html>
   );
 }
